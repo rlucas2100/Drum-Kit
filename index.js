017dc500903570e84cee5285fe3b268cb69ec84a
Ryan Lucas <ryan@Ryans-MacBook-Pro.local>
2024-10-16 14:21:21 +0100

    Add event listener

---
+++ index.js
@@ -0,0 +1,13 @@
+let numberOfDrumButtons = document.querySelectorAll(".drum").length; 
+
+for (var i = 0; i<numberOfDrumButtons; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function ()  {
+        alert("I got clicked");
+    });
+
+}
+
+
+
+

3244ffbf23d7f51b3f483bcb61f27dce3e6999ff
Ryan Lucas <ryan@Ryans-MacBook-Pro.local>
2024-10-18 14:10:47 +0100

    Add background images to drums

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 let numberOfDrumButtons = document.querySelectorAll(".drum").length; 
 
+
+// add event listener to all buttons
 for (var i = 0; i<numberOfDrumButtons; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function ()  {
-        alert("I got clicked");
+        let audio = new Audio('sounds/tom-1.mp3');
+        // audio object
+        audio.play();
     });
+
 
 }
 
+let pie = ['blue', 'red', 'green'];
 
+console.log(pie[0]);
+console.log(pie[2]);
 
 
